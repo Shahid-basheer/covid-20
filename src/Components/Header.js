084fb4a2c1b76c6459a9c7cc75ed62674fed3b53
Shahid basheer <muhammadshahid4127@gmail.com>
2023-07-19 13:21:37 +0100

    Changed ui style

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,22 +1,28 @@
-import { Link } from 'react-router-dom'
-import { Nav, Navbar } from 'react-bootstrap'
+import { Link } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
 const Header = () => {
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      style={{ width: "100%", position: "fixed", zIndex: "1" }}
+    >
+      <Navbar.Brand href="#home">Covid 19</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Link to="/india" className="nav-link">
+            India
+          </Link>
+          <Link to="/world" className="nav-link">
+            World
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-    return (
-        <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{width:'100%',position:'fixed',zIndex:'1'}}>
-                <Navbar.Brand href="#home">Covid 19</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Link to="/india" className='nav-link'>India</Link>
-                        <Link to="/world" className='nav-link'>World</Link>
-
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
-    )
-}
-
-export default Header
+export default Header;
--- src/Components/World.js
+++ src/Components/World.js
@@ -1,60 +1,73 @@
-import './World.css'
-import { Table } from 'react-bootstrap'
-import React, { useState, useEffect } from 'react'
-import Axios from 'axios'
+import "./World.css";
+import { Table } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 const World = () => {
-    const [state, setState] = useState([])
-    useEffect(() => {
-        Axios.get('https://corona.lmao.ninja/v2/countries').then((res) => {
-            setState(res.data)
-        })
-    }, [])
-    return (
-        <div className='world'>
-       
-            <h3>World</h3>
-            <div className="container mt-5 pt-5" style={{maxHeight:'600px', overflowY:' auto'}}>
-            <Table striped bordered hover variant="dark" responsive >
-            <thead>
+  const [state, setState] = useState([]);
+  useEffect(() => {
+    Axios.get("https://corona.lmao.ninja/v2/countries").then((res) => {
+      setState(res.data);
+    });
+  }, []);
+  return (
+    <div className="world" style={{ paddingTop: "80px" }}>
+      {state.length > 0 ? (
+        <>
+          <h3>World</h3>
+          <div
+            className="container mt-5 pt-5 scroll"
+            style={{ maxHeight: "600px", overflowY: " auto" }}
+          >
+            <Table striped bordered hover variant="dark" responsive>
+              <thead>
                 <tr>
-                    <th>No</th>
-                    <th>Flags</th>
-                    <th>Country</th>
-                    <th>Total Case</th>
-                    <th>Recovered</th>
-                    <th>Death</th>
+                  <th>No</th>
+                  <th>Flags</th>
+                  <th>Country</th>
+                  <th>Total Case</th>
+                  <th>Recovered</th>
+                  <th>Death</th>
                 </tr>
-            </thead>
-            {
-                state.map((item, key) => {
-                    return (
-                        <tbody>
-                        <tr>
-                            <td>{key+1}</td>
-                           <td><img src={item.countryInfo.flag} alt="" style={{width:'30px'}} />
-                           </td>                            
-                           <td>{item.country}</td>
-                            <td>{item.cases}</td>
-                            <td>{item.recovered}</td>
-                            <td>{item.deaths}</td>
-                        </tr>
-        
-                    </tbody>
-                    
-                    )
-
-                })
-            }
+              </thead>
+              {state.map((item, key) => {
+                return (
+                  <tbody>
+                    <tr>
+                      <td>{key + 1}</td>
+                      <td>
+                        <img
+                          src={item.countryInfo.flag}
+                          alt=""
+                          style={{ width: "30px" }}
+                        />
+                      </td>
+                      <td>{item.country}</td>
+                      <td>{item.cases}</td>
+                      <td>{item.recovered}</td>
+                      <td>{item.deaths}</td>
+                    </tr>
+                  </tbody>
+                );
+              })}
             </Table>
-       
-                
-
-            </div>
-
+          </div>
+        </>
+      ) : (
+        <div
+          style={{
+            width: "100%",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>Loading..</h1>
         </div>
-        
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default World
+export default World;
